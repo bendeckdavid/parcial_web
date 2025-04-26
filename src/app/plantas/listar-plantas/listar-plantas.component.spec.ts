f0e7c27acc167e808c89f70a515f37eb885797bc
BendeckDavid <davidgbendeck@gmail.com>
2025-04-25 21:21:20 -0500

    Tarea: 3.1 Test con 3 Plantas

--- src/app/plantas/listar-plantas/listar-plantas.component.spec.ts
+++ src/app/plantas/listar-plantas/listar-plantas.component.spec.ts
@@ -21,4 +21,24 @@ describe('ListarPlantasComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('muestra las plantas en la tabla', () => {
+    component.data = Array.from({ length: 3 }, (v, k) => new Planta(
+      k, // id
+      faker.word.words(2), // nombre_comun
+      faker.word.words(3), // nombre_cientifico
+      faker.helpers.arrayElement(['Interior', 'Exterior']), // tipo
+      faker.number.int({ min:10, max:1000 }), // altura_maxima
+      faker.helpers.arrayElement([
+        faker.helpers.arrayElements(['Frío', 'Templado', 'cálido'], { min:1, max:2 }),
+        ['Todos']
+      ]).join(', '), // clima
+      faker.lorem.sentence(10) // sustrato_siembra
+    ));
+
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const rows = compiled.querySelectorAll('tbody tr');
+    expect(rows.length).toBe(3);
+  });
+
 });
